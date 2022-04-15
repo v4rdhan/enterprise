dda46ccb657ad921bf3397ae052942302d876cb9
v4rdhan <harsha.vrx@gmail.com>
2022-04-15 17:06:52 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,3 +2,19 @@
 
 console.log(`Hello Node.js v${process.versions.node}!`);
 
+const {Client} = required('pg')
+
+const client = new Client({
+  host: "ec2-34-197-84-74.compute-1.amazonaws.com",
+  database: "d2v1aj6saamsi9",
+  user: "rmycdniqxhowpv",
+  port: 5432,
+  password: "deb0c16207eb6679f5a1d0f7709b2f7db58fe7c3d8b0b560c2652c25e67ee6a9"
+})
+
+client.connec();
+
+client.query(`SELECT NOW()`, (err, res) => {
+  console.log(err,res);
+  client.end;
+})
